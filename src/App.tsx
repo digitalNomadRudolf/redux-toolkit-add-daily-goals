2527b0af20cd5165b94b793aafffbbc7779d9bad
Rudolf Cardol <r_cardol@hotmail.com>
2025-03-19 21:22:23 +0000

    Setup project and add components

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="portfolio-page">
+      <RandomBackground />
+      <WeatherDisplay />
+      <RandomQuote />
+      <AddGoal />
+      <GoalList />
+    </div>
+  );
+}
+
+export default App;
