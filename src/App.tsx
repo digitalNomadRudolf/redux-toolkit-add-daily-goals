04ad413159e8e80121c23494c3aeaf60b1f3b66e
Rudolf Cardol <r_cardol@hotmail.com>
2025-03-20 19:40:14 +0000

    refactor app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
+import AddGoal from "./components/AddGoal";
 import RandomBackgroundContainer from "./components/RandomBackgroundContainer";
+import RandomQuote from "./components/RandomQuote";
 import WeatherDisplay from "./components/WeatherDisplay";
+import GoalList from "./features/goals/GoalList";
 
 function App() {
   return (
-    <div className="portfolio-page">
-      <WeatherDisplay />
+    <>
       <RandomBackgroundContainer />
-      {/* <RandomQuote /> */}
-      {/* <AddGoal /> */}
-      {/* <GoalList />  */}
-    </div>
+      <div className="header">
+        <WeatherDisplay />
+      </div>
+      <div className="portfolio-page flex flex-col align-center w-[85%] mx-auto gap-y-8">
+        <RandomQuote />
+        <AddGoal />
+        <GoalList />
+      </div>
+    </>
   );
 }
 
