505bf72e170e4d8cbb7f9d61116e4dbd9f340b5d
Rudolf Cardol <r_cardol@hotmail.com>
2025-03-21 13:11:43 +0000

    add slice to store

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import goalsReducer from "./features/goals/goalsSlice";
 import weatherReducer from "./features/weather/weatherSlice";
+import quoteReducer from "./features/quotes/quotesSlice";
 
 const store = configureStore({
   reducer: {
     goals: goalsReducer,
     weather: weatherReducer,
+    quote: quoteReducer,
   },
 });
 
