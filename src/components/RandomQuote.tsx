cca64a80fdc8c2903adb392d2c0585494ecd5431
Rudolf Cardol <r_cardol@hotmail.com>
2025-03-21 13:12:40 +0000

    refactor RandomQuote
    quote is now fetched through the state

--- src/components/RandomQuote.tsx
+++ src/components/RandomQuote.tsx
@@ -1,12 +1,27 @@
-import quotesData from "../data/quotes.json";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
 
 const RandomQuote = () => {
-  const quotes = quotesData.quotes;
-  const randomNum = Math.floor(Math.random() * quotes.length);
+  const {
+    quote: fetchedQuote,
+    isLoading,
+    error,
+  } = useSelector((state: RootState) => state.quote);
+  // format quote as some quotes have each word capitalized
+  const formattedQuote =
+    fetchedQuote.charAt(0).toUpperCase() + fetchedQuote.slice(1).toLowerCase();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Something went wrong fetching the quote...</div>;
+  }
 
   return (
     <div className="random-quote drop-shadow-lg p-6 relative z-10 w-full mx-auto text-white text-2xl flex justify-center">
-      {quotes[randomNum].text}
+      "{formattedQuote}"
     </div>
   );
 };
