f5e0394958c8600512f83aa3be820647f32b1ab7
Rudolf Cardol <r_cardol@hotmail.com>
2025-03-21 21:59:08 +0000

    get goals from state

--- src/features/goals/GoalList.tsx
+++ src/features/goals/GoalList.tsx
@@ -1,11 +1,9 @@
-import goalData from "../../data/goals.json";
+import { useSelector } from "react-redux";
 import Goal from "./Goal";
+import { RootState } from "../../store";
 
 const GoalList = () => {
-  const goals = goalData.goals;
-  // return a div that goes through the goals and renders them
-  // TODO: Right now the goals are fetched from a json file.
-  // TODO: Goals will be added through the addGoal input sending a dispatch to add a goal
+  const { goals } = useSelector((state: RootState) => state.goals);
   return (
     <div className="goals-list relative text-white flex flex-wrap justify-center p-6 mx-auto border-white border-2 w-full bg-white/25">
       {goals.map((goal) => (
