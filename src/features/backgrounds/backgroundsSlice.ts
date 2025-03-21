415c3d8c00aa713c984e0f9f9b6b0494538ae717
Rudolf Cardol <r_cardol@hotmail.com>
2025-03-21 16:15:31 +0000

    add backgroundsSLice

---
+++ src/features/backgrounds/backgroundsSlice.ts
@@ -0,0 +1,49 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { BackgroundType } from "../../components/RandomBackground";
+
+const accessKey = import.meta.env.VITE_UNSPLASH_ACCESS_KEY;
+
+export const fetchBackgrounds = createAsyncThunk(
+  "backgrounds/fetchBackgrounds",
+  async () => {
+    const response = await fetch(
+      `https://api.unsplash.com/photos/random?client_id=${accessKey}&count=5`
+    );
+    const data = await response.json();
+    console.log({ data });
+    return data;
+  }
+);
+
+const backgroundSlice = createSlice({
+  name: "backgrounds",
+  initialState: {
+    backgrounds: [] as BackgroundType[],
+    currentBackgroundIndex: 0,
+    isLoading: false,
+    error: null as string | null,
+  },
+  reducers: {
+    setCurrentBackgroundIndex: (state, action) => {
+      state.currentBackgroundIndex = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBackgrounds.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchBackgrounds.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.backgrounds = action.payload;
+        state.currentBackgroundIndex = 0;
+      })
+      .addCase(fetchBackgrounds.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message || "Failed to fetch background";
+      });
+  },
+});
+
+export const { setCurrentBackgroundIndex } = backgroundSlice.actions;
+export default backgroundSlice.reducer;
--- src/store.ts
+++ src/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import goalsReducer from "./features/goals/goalsSlice";
 import weatherReducer from "./features/weather/weatherSlice";
 import quoteReducer from "./features/quotes/quotesSlice";
+import backgroundReducer from "./features/backgrounds/backgroundsSlice";
 
 const store = configureStore({
   reducer: {
     goals: goalsReducer,
     weather: weatherReducer,
     quote: quoteReducer,
+    backgrounds: backgroundReducer,
   },
 });
 
