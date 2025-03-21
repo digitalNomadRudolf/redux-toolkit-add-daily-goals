8f18b1e76b6ca2037767331656dff1ab149d19d0
Rudolf Cardol <r_cardol@hotmail.com>
2025-03-21 21:59:34 +0000

    add buttons to remove goal & toggle goal done

--- src/features/goals/Goal.tsx
+++ src/features/goals/Goal.tsx
@@ -1,13 +1,36 @@
+import { useDispatch } from "react-redux";
+import { removeGoal, toggleDone } from "./goalsSlice";
+
 interface GoalProps {
-  id: number;
+  id: string;
   name: string;
   isDone: boolean;
 }
 
-const Goal = ({ name }: GoalProps) => {
+const Goal = ({ id, name, isDone }: GoalProps) => {
+  const dispatch = useDispatch();
+
   return (
-    <div className="goal p-6 border-2 text-xl rounded m-2 bg-white text-blue-500">
+    <div
+      className={`goal relative group p-6 border-2 text-xl rounded m-2 bg-white text-blue-500 ${
+        isDone ? "opacity-10" : "opacity-100"
+      }`}
+    >
       {name}
+      <div className="absolute -top-5 -right-10 flex text-xs gap-2 z-100">
+        <button
+          className="opacity-0 group-hover:opacity-100 !bg-blue-500 hover:!bg-blue-200 text-white"
+          onClick={() => dispatch(removeGoal(id))}
+        >
+          Remove
+        </button>
+        <button
+          className="opacity-0 group-hover:opacity-100 !bg-blue-100"
+          onClick={() => dispatch(toggleDone(id))}
+        >
+          Done
+        </button>
+      </div>
     </div>
   );
 };
